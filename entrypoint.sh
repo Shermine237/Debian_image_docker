9ae1646413be24c5b7a6aa41c838f03398d7e272
Charlie Rostant YOSSA <charlieyossa@gmail.com>
2023-06-02 13:22:11 +0100

    Add bash command terminal to entrypoint.sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #bash commands here
-echo Hello $1, you\'re logged as $(whoami). \nCurrent directory: $(pwd)
-ls -al
+echo Hello $1, you\'re logged as $(whoami). Current directory: $(pwd)
+bash
